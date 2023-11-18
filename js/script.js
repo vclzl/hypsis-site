fcfb85f9acebfa01dca2633eedeb78c956f66b45
vclzl <valere.clauzel@ik.me>
2023-11-18 21:51:15 +0100

    correction script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     let scrollPos = 0;
-    const nav = document.querySelector('#header-main');
+    const nav = document.querySelector('#header-socials');
     function checkPosition() {
         let windowY = window.scrollY;
         if (windowY < scrollPos) {
@@ -14,3 +14,21 @@ window.onload = function() {
         }
         scrollPos = windowY;
     }
+    window.addEventListener('scroll', checkPosition);
+    let footnotes = document.querySelectorAll(".footnotes ol li");
+    for (let i = 0; i < footnotes.length; i++) {
+        let clone = document.createElement("div");
+        clone.innerHTML = footnotes[i].innerHTML;
+        clone.classList.add("margin-note");
+        let footRef = document.querySelectorAll(".footnote-ref")[i].parentNode;
+        footRef.parentNode.insertBefore(clone, footRef.nextSibling);
+    }
+    let backref = document.querySelectorAll(".margin-note .footnote-backref");
+    for (let b = 0; b < backref.length; b++) {
+        backref[b].remove();
+    }
+    orthotypo(document.body);
+    spaces(document.body);
+    exposants();
+    noHyphens();
+}
